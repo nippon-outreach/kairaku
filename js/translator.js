ca7ce526a71c75bc99ba0292de56da80fc6291db
nippon-outreach <nippon.outreach.web@gmail.com>
2020-03-16 15:41:17 +0800

    updates on i18n

--- js/translator.js
+++ js/translator.js
@@ -1,46 +1,32 @@
 // get current url
-var url = window.location.protocol + "//" + window.location.host + window.location.pathname;
+// var url = window.location.protocol + "//" + window.location.host + window.location.pathname;
+// var newurl = url + '?locale=en';
+// window.history.pushState({ path: newurl }, '', newurl);
+
+function getLocale(){
+    return sessionStorage.getItem('locale');
+}
+
+function setLocale(value){
+    return sessionStorage.setItem('locale', value);
+}
 
 // toggle current locale
 function toggleLocale(){
-    var locale = sessionStorage.getItem('locale');
-    console.log('current session local: ', locale);
-
-    if (locale === 'jp') {
-        var newurl = url + '?locale=en';
-        window.history.pushState({ path: newurl }, '', newurl);
-
-        sessionStorage.setItem('locale', 'en');
-        console.log('new session local: ', locale);
-    } else if (locale === 'en'){
-        var newurl = url + '?locale=jp';
-        window.history.pushState({ path: newurl }, '', newurl);
-
-        sessionStorage.setItem('locale', 'jp');
-        console.log('new session local: ', locale);
-    }
+    getLocale() === 'jp' ? setLocale('en') : setLocale('jp');
 }
 
 function main(){
     // check session storage
-    var locale = sessionStorage.getItem('locale');
-    console.log('current session local: ', locale);
+    var locale = getLocale();
     
     if (!locale) {
         locale = 'en';
-
-        var newurl = url + '?locale=en';
-        window.history.pushState({ path: newurl }, '', newurl);
-
-        sessionStorage.setItem('locale', 'en');
-        console.log('new session local: ', locale);
+        setLocale('en');
     }
 
     arb.setResourceSelector(locale);
-
-    // This should appear after all the translatable HTML content
     arb.localizeHtml();
 }
-
 
 main();
